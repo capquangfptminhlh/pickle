f42065c02c4f8c52dd1bb9fefedced4ebf2c752d
capquangfptminhlh <capquangfpt.minhlh@gmail.com>
2026-09-24 13:48:50 +0700

    preview: sync native app V3 cache

--- preview/service-worker.js
+++ preview/service-worker.js
@@ -1,4 +1,4 @@
-const VERSION="pickle-preview-v10-appshell-20260924";
+const VERSION="pickle-preview-v11-native-v3-20260924";
 const CORE=[
   "./index.html","./admin.html","./ranking.html","./tournament.html","./player.html","./checkin.html","./about.html","./offline.html",
   "./preview-api.js","./preview-public.js","./preview-pwa.js",
