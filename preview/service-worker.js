20a09b43ff4b73d43f1b9898155b4663ffb78fa4
capquangfptminhlh <capquangfpt.minhlh@gmail.com>
2026-09-24 18:11:03 +0700

    preview: sync premium mobile redesign

--- preview/service-worker.js
+++ preview/service-worker.js
@@ -1,4 +1,4 @@
-const VERSION="pickle-preview-v13-clean-native-20260924";
+const VERSION="pickle-preview-premium-v2-237847";
 const CORE=[
   "./index.html","./admin.html","./ranking.html","./tournament.html","./player.html","./checkin.html","./about.html","./offline.html",
   "./preview-api.js","./preview-public.js","./preview-pwa.js",
