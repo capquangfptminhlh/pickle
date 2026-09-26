e7c0f91def8496def5820d5bade65e07382290b1
capquangfptminhlh <capquangfpt.minhlh@gmail.com>
2026-09-26 10:43:17 +0700

    preview: refresh cache after mobile fixes

--- preview/service-worker.js
+++ preview/service-worker.js
@@ -1,4 +1,4 @@
-const VERSION="pickle-pages-v14-community-app";
+const VERSION="pickle-pages-v15-mobile-fixes";
 const CORE=[
   "./index.html","./login.html","./admin.html","./ranking.html","./tournament.html","./player.html","./checkin.html","./about.html","./offline.html",
   "./preview-api.js","./preview-public.js","./preview-pwa.js","./preview-guard.js",
