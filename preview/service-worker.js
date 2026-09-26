3a1590dd0e7133baaec3d7579275cfad39b6036b
capquangfptminhlh <capquangfpt.minhlh@gmail.com>
2026-09-26 10:31:31 +0700

    preview: publish community app UI cache

--- preview/service-worker.js
+++ preview/service-worker.js
@@ -1,4 +1,4 @@
-const VERSION="pickle-pages-v13-community-ui";
+const VERSION="pickle-pages-v14-community-app";
 const CORE=[
   "./index.html","./login.html","./admin.html","./ranking.html","./tournament.html","./player.html","./checkin.html","./about.html","./offline.html",
   "./preview-api.js","./preview-public.js","./preview-pwa.js","./preview-guard.js",
